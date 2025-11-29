54f1c667f032b218d37fc82f73ee1ca6ec1aefe7
Калоян Димитров <136224811+LuckyKaloyan@users.noreply.github.com>
2025-11-30 00:25:55 +0200

    Base up running up, tomorrow big.

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home(){
+
+return (
+    <section>
+    <main>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio expedita in quae nostrum quia inventore veniam velit commodi, iste, ad deleniti cum aliquid voluptate reiciendis minima voluptatum odit rerum laudantium.</p>
+    </main>
+</section>
+)
+
+}
